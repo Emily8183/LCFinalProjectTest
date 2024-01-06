a23e61ad879014e140eb9dbaf51aca3afdb98851
Emily <emily.zy.lin@gmail.com>
2024-01-06 15:50:02 -0600

    corrected the homecontroller's package

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/controller/HomeController.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.codechampions.easytravel.controller;
+package com.skilldev.easytraveltest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
