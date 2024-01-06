6126923cc94db6e857fcd087008d5763a1437e92
Emily <emily.zy.lin@gmail.com>
2024-01-06 14:33:51 -0600

    spring initializer

---
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/EasytraveltestApplication.java
@@ -0,0 +1,13 @@
+package com.skilldev.easytraveltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytraveltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasytraveltestApplication.class, args);
+	}
+
+}
---
+++ easytraveltest/src/test/java/com/skilldev/easytraveltest/EasytraveltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldev.easytraveltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasytraveltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
