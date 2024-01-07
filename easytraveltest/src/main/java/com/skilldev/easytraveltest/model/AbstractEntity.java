23eba9ce271941b362f4a4ef6c0de9cefee8c341
Emily <emily.zy.lin@gmail.com>
2024-01-06 22:05:48 -0600

    added AbstractEntity

---
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/model/AbstractEntity.java
@@ -0,0 +1,14 @@
+package com.skilldev.easytraveltest.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+}
