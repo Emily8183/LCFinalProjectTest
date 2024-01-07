534c1895c848390da4dfa13307e3699df96e0633
Emily <emily.zy.lin@gmail.com>
2024-01-06 22:10:54 -0600

    added username in user model

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/model/User.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/model/User.java
@@ -1,9 +1,15 @@
 package com.skilldev.easytraveltest.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 public class User extends AbstractEntity{
+
+    @NotNull
+    private String username;
+
+
 
 }
