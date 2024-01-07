a1053fd39ed38a536561d0af89f1f6fa4a08105b
Emily <emily.zy.lin@gmail.com>
2024-01-07 12:20:52 -0600

    added GetMapping in AuthenticationController to return the page of registration_form

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/controller/AuthenticationController.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/controller/AuthenticationController.java
@@ -1,9 +1,16 @@
 package com.skilldev.easytraveltest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
 
+    @GetMapping("/register")
+    public String showRegistrationForm(model model) {
+        model.addAttribution("user", new User());
+
+        return "registration_form";
+    }
 
 }
