b556a8f1e6b41096b1de701bcc321dda81648ea0
Emily <emily.zy.lin@gmail.com>
2024-01-06 22:42:39 -0600

    added UserRepositoryTests (not finished)

---
+++ easytraveltest/src/test/java/com/skilldev/easytraveltest/UserRepositoryTests.java
@@ -0,0 +1,24 @@
+package com.skilldev.easytraveltest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.skilldev.easytraveltest.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+}
