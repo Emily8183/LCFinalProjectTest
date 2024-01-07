5322efbd03ad75c9cbaa0ee399003da55c1d4749
Emily <emily.zy.lin@gmail.com>
2024-01-07 15:10:56 -0600

    added model addAttribute to showRegistrationForm handler

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/controller/AuthenticationController.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/controller/AuthenticationController.java
@@ -1,16 +1,35 @@
 package com.skilldev.easytraveltest.controller;
 
+import com.skilldev.easytraveltest.model.User;
+import com.skilldev.easytraveltest.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
 
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/register")
-    public String showRegistrationForm() {
-//        model.addAttribution("user", new User());
+    public String showRegistrationForm(Model model) {
+        //TODO: add PTO
+        //TODO: change the return to @PostMapping to activity page
+
+        model.addAttribute("user", new User());
 
         return "registration_form";
     }
 
+    @PostMapping("/register")
+    public String processRegister(User newUser) {
+
+        userRepository.save(newUser);
+//        return "redirect:/activites";
+        return "index";
+    }
 }
