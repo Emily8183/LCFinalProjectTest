76e6a94671b76fc3c6d094afb3c853647cfdba83
Emily <emily.zy.lin@gmail.com>
2024-01-08 09:52:33 -0600

    added username, password, firstName in LoginFormDTO.java

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/model/dto/LoginFormDTO.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/model/dto/LoginFormDTO.java
@@ -1,4 +1,22 @@
 package com.skilldev.easytraveltest.model.dto;
 
 public class LoginFormDTO {
+
+    @NotNull(message = "Username is required")
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be 3-20 characters Long.")
+    private String username;
+
+    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 30, message = "Password must be 6-30 characters long.")
+    private String password;
+
+    @NotNull(message = "First Name is required")
+    @NotBlank(message = "First Name is required")
+    private String firstName;
+
+
+
+
 }
