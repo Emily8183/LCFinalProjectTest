f1e2ad057cc264b2d0d81386a3bf63c0fa5e3acb
Emily <emily.zy.lin@gmail.com>
2024-01-08 10:56:35 -0600

    revised User model

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/model/User.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/model/User.java
@@ -7,51 +7,53 @@ import org.antlr.v4.runtime.misc.NotNull;
 @Entity
 public class User extends AbstractEntity{
 
-    @NotNull
-    @Column(unique = true)
-    private String username;
+    public User() {}
 
-    @NotNull
-    @Column(length = 64)
-    private String password;
-
-    @NotNull
-    @Column(name = "first_name", length = 20)
-    private String firstName;
-
-    @NotNull
-    @Column(name = "last_name", length = 20)
-    private String lastName;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+//    @NotNull
+//    @Column(unique = true)
+//    private String username;
+//
+//    @NotNull
+//    @Column(length = 64)
+//    private String password;
+//
+//    @NotNull
+//    @Column(name = "first_name", length = 20)
+//    private String firstName;
+//
+//    @NotNull
+//    @Column(name = "last_name", length = 20)
+//    private String lastName;
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
 }
