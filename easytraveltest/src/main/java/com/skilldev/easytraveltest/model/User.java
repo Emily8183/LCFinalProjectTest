58760a51580f2826898deeb7579c4ba5c207e235
Emily <emily.zy.lin@gmail.com>
2024-01-09 14:20:20 -0600

    added encoder to user model

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/model/User.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/model/User.java
@@ -12,13 +12,13 @@ public class User extends AbstractEntity{
 //    @NotNull
     private String username;
 
-    private String password;
+    private String pwHash;
 
     public User() {}
 
     public User(String username, String password) {
         this.username = username;
-        this.pwHash = password;
+        this.pwHash = encoder.encode(password);
     }
 
     //
@@ -34,20 +34,9 @@ public class User extends AbstractEntity{
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
+    //not able to be overwritten
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-//
 //    public String getFirstName() {
 //        return firstName;
 //    }
