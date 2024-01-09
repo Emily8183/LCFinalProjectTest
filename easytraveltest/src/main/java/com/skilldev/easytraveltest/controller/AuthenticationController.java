4c433aed2a3ac93e910b49bda3e7dcba61211115
Emily <emily.zy.lin@gmail.com>
2024-01-09 09:49:22 -0600

    cleaned authenticationController and HomeController

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/controller/AuthenticationController.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/controller/AuthenticationController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -19,7 +18,6 @@ public class AuthenticationController {
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
-        //TODO: add PTO
 
         model.addAttribute(new RegistrationFormDTO());
 
