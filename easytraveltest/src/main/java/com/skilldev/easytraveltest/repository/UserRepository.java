0eecc161ef8ea62622b379b2120803be57272a67
Emily <emily.zy.lin@gmail.com>
2024-01-09 20:15:24 -0600

    temporarily removed ProfileRepository.java

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/repository/UserRepository.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/repository/UserRepository.java
@@ -1,12 +1,12 @@
-//package com.skilldev.easytraveltest.repository;
-//
-//import com.skilldev.easytraveltest.model.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Integer> {
-//    User findByUsername(String username);
-//}
+package com.skilldev.easytraveltest.repository;
+
+import com.skilldev.easytraveltest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
