f1421a46f8dfe9dceeef1c3d30672dc8c0067ce8
Emily <emily.zy.lin@gmail.com>
2024-01-09 19:46:20 -0600

    added firstname, lastname, address, location, email to User model

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/model/User.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/model/User.java
@@ -14,6 +14,19 @@ public class User extends AbstractEntity{
 
     private String pwHash;
 
+
+//    @NotNull
+    private String firstName;
+
+//    @NotNull
+    private String lastName;
+
+    private String address;
+
+    private String location;
+
+    private String email;
+
     public User() {}
 
     public User(String username, String password) {
@@ -21,37 +34,62 @@ public class User extends AbstractEntity{
         this.pwHash = encoder.encode(password);
     }
 
-    //
-//    @NotNull
-//    @Column(name = "first_name", length = 20)
-//    private String firstName;
-//
-//    @NotNull
-//    @Column(name = "last_name", length = 20)
-//    private String lastName;
-//
+    public User(String firstName, String lastName, String address, String location, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.location = location;
+        this.email = email;
+    }
+
     public String getUsername() {
         return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     //not able to be overwritten
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
+
 
     public boolean isMatchingPassword(String password) {
         return encoder.matches(password, pwHash);
