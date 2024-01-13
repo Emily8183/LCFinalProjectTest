a15c77d8e8731ae7942e9adf8ba7a124bacb0990
Emily <emily.zy.lin@gmail.com>
2024-01-13 15:43:34 -0600

    updated ActivityRepository

--- easytraveltest/src/main/java/com/skilldev/easytraveltest/repository/ActivityRepository.java
+++ easytraveltest/src/main/java/com/skilldev/easytraveltest/repository/ActivityRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ActivityRepository extends JpaRepository<Activity,Long> {
+public interface ActivityRepository extends JpaRepository<Activity,Integer> {
 
-    List<Activity> findByGroupId(Long groupId);
 }
